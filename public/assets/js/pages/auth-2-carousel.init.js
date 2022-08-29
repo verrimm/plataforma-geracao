3f0e87d26635209bce6d9677f36ef57239780f2e
Mateus Vicenzo Silveira Giordani - Unicred Central RS <mateus.g.0999@e-unicred.com.br>
2022-08-29 16:10:52 -0300

    alterações gitignore

--- public/assets/js/pages/auth-2-carousel.init.js
+++ public/assets/js/pages/auth-2-carousel.init.js
@@ -1,1 +1,14 @@
-$("#auth-review-carousel").owlCarousel({items:1,loop:!1,margin:16,nav:!1,dots:!0});
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************************************!*\
+  !*** ./resources/js/pages/auth-2-carousel.init.js ***!
+  \****************************************************/
+$("#auth-review-carousel").owlCarousel({
+  items: 1,
+  loop: !1,
+  margin: 16,
+  nav: !1,
+  dots: !0
+});
+/******/ })()
+;
