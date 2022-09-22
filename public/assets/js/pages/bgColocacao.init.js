0a9fe235a58c6edacbefed0f5ecc73b6bd3a3fa8
mateusgiordani-unicred <102830440+mateusgiordani-unicred@users.noreply.github.com>
2022-09-22 13:41:29 -0300

    coisas laravel

--- public/assets/js/pages/bgColocacao.init.js
+++ public/assets/js/pages/bgColocacao.init.js
@@ -3,25 +3,28 @@ var __webpack_exports__ = {};
 /*!************************************************!*\
   !*** ./resources/js/pages/bgColocacao.init.js ***!
   \************************************************/
-var posicao = document.querySelector(".posicaoRanking");
-var bgIndicador = document.querySelector(".bgIndicador");
+function bgColocacao() {
+  // <!-- define cor da posição ranking  -->
+  var posicao = document.querySelector(".posicaoRanking");
+  var bgIndicador = document.querySelector(".bgIndicador");
 
-switch (posicao.innerText) {
-  case "1º":
-    bgIndicador.classList.add("bg-ouro");
-    break;
+  switch (posicao.innerText) {
+    case "1º":
+      bgIndicador.classList.add("bg-ouro");
+      break;
 
-  case "2º":
-    bgIndicador.classList.add("bg-prata");
-    break;
+    case "2º":
+      bgIndicador.classList.add("bg-prata");
+      break;
 
-  case "3º":
-    bgIndicador.classList.add("bg-bronze");
-    break;
+    case "3º":
+      bgIndicador.classList.add("bg-bronze");
+      break;
 
-  default:
-    bgIndicador.classList.add("bg-ranking");
-    break;
+    default:
+      bgIndicador.classList.add("bg-ranking");
+      break;
+  }
 }
 /******/ })()
 ;
--- public/assets/js/pages/progressMeta.init.js
+++ public/assets/js/pages/progressMeta.init.js
@@ -3,25 +3,27 @@ var __webpack_exports__ = {};
 /*!*************************************************!*\
   !*** ./resources/js/pages/progressMeta.init.js ***!
   \*************************************************/
-var progress = document.querySelector(".js-completed-bar");
+function progressMeta() {
+  var progress = document.querySelector(".js-completed-bar");
 
-if (progress) {
-  progress.style.width = progress.getAttribute("data-complete") - 2 + "%";
-  progress.style.opacity = 1;
-}
+  if (progress) {
+    progress.style.width = progress.getAttribute("data-complete") - 2 + "%";
+    progress.style.opacity = 1;
+  }
 
-var marcosMetaSegundo = document.querySelector(".marcosMetaSegundo");
+  var marcosMetaSegundo = document.querySelector(".marcosMetaSegundo");
 
-if (marcosMetaSegundo) {
-  marcosMetaSegundo.style.paddingLeft = marcosMetaSegundo.getAttribute("data-complete-meta") - 2 + "%";
-  marcosMetaSegundo.style.opacity = 1;
-}
+  if (marcosMetaSegundo) {
+    marcosMetaSegundo.style.paddingLeft = marcosMetaSegundo.getAttribute("data-complete-meta") - 2 + "%";
+    marcosMetaSegundo.style.opacity = 1;
+  }
 
-var marcosMetaTerceiro = document.querySelector(".marcosMetaTerceiro");
+  var marcosMetaTerceiro = document.querySelector(".marcosMetaTerceiro");
 
-if (marcosMetaTerceiro) {
-  marcosMetaTerceiro.style.paddingLeft = marcosMetaTerceiro.getAttribute("data-complete-meta") - 4 + "%";
-  marcosMetaTerceiro.style.opacity = 1;
+  if (marcosMetaTerceiro) {
+    marcosMetaTerceiro.style.paddingLeft = marcosMetaTerceiro.getAttribute("data-complete-meta") - 4 + "%";
+    marcosMetaTerceiro.style.opacity = 1;
+  }
 }
 /******/ })()
 ;
